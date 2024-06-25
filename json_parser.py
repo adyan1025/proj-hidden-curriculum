866d80c68808a5aab9f25e139b160adcdcb2ae76
Adyan Chowdhury <chowdhuryadyan13@gmail.com>
2024-06-25 15:12:58 -0400

    proj-hidden-curriculum

---
+++ json_parser.py
@@ -0,0 +1,16 @@
+import json
+import csv
+
+with open('History.json', 'r') as f:
+    browser_history = json.load(f)
+csv_file = "tiktoks.csv"
+
+index = 0
+with open(csv_file, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Index', 'Link'])
+    for browser_data in browser_history["Browser History"]:
+        url = str(browser_data["url"])
+        if "video" in url:
+            index = index + 1
+            writer.writerow([index, url])
